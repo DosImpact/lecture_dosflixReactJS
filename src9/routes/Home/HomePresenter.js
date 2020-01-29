696caac95ba3c4201b84501ab062793786260530
DosImpact <ypd03008@gmail.com>
2020-01-29 18:19:19 +0900

    src9 - gird+flex system

--- src9/routes/Home/HomePresenter.js
+++ src9/routes/Home/HomePresenter.js
@@ -24,16 +24,39 @@ const GridContent = styled.article`
 const GridItem = styled.div`
   border-radius: 30px;
   & img {
-    height: 30px;
-    width: 30px;
+    border-radius: 30px;
+    height: 50%;
+    width: 100%;
   }
   &:nth-child(1) {
-    background-image: url(https://i.pinimg.com/564x/b8/c0/7a/b8c07a5a63f15415d036ea209abee11d.jpg);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+      url(https://i.pinimg.com/564x/b8/c0/7a/b8c07a5a63f15415d036ea209abee11d.jpg);
     grid-column: span 2;
     background-position: center;
     background-size: cover;
+    color: white;
+    font-size: 30px;
+    padding: 10px;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    & div:nth-child(1) {
+      justify-self: flex-end;
+      grid-row: span 2;
+    }
+    & div:nth-child(2) {
+      justify-self: flex-start;
+    }
+    & div:nth-child(3) {
+      justify-self: flex-end;
+    }
+  }
+  &:nth-child(2) {
+  }
+  &:nth-child(3) {
   }
 `;
+const FlexItem = styled.div``;
 
 export default () => {
   return (
@@ -42,40 +65,63 @@ export default () => {
       <SubTitle>Simple knowledge to learn quickly.</SubTitle>
       <GridContent>
         <GridItem>
-          <div>Daily language</div>
-          <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          <div>
+            Daily<br></br> language
+          </div>
+          <div>Lorem ipsum, dolor sit amet </div>
+          <div>consectetur adipisicing elit.</div>
         </GridItem>
         <GridItem>
-          <img
-            src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
-            alt="example"
-          />
-          <div>Daily language</div>
-          <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
         </GridItem>
         <GridItem>
-          <img
-            src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
-            alt="example"
-          />
-          <div>Daily language</div>
-          <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
-        </GridItem>
-        <GridItem>
-          <img
-            src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
-            alt="example"
-          />
-          <div>Daily language</div>
-          <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
-        </GridItem>
-        <GridItem>
-          <img
-            src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
-            alt="example"
-          />
-          <div>Daily language</div>
-          <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
+          <FlexItem>
+            <img
+              src="https://i.pinimg.com/564x/b6/28/6e/b6286e4555719127578b4e9eb3a09b50.jpg"
+              alt="example"
+            />
+            <div>Daily language</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+          </FlexItem>
         </GridItem>
       </GridContent>
     </HomeContent>

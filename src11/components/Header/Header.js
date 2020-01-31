7a537c9f8046b5d42746510f8d8bc41297351f42
DosImpact <ypd03008@gmail.com>
2020-01-31 23:09:57 +0900

    del antd src11

--- src11/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { withRouter, Link } from "react-router-dom";
-export default withRouter(({ location: { pathname } }) => (
-  <header>
-    {console.log(pathname)}
-    <Link to="/">Home</Link>
-    <Link to="/detail">Detail</Link>
-  </header>
-));

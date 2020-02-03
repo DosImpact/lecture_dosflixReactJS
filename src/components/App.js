a0ba2feb2c0e443a9b90836978b707fe077a8644
DosImpact <ypd03008@gmail.com>
2020-02-03 12:05:53 +0900

    GlobalStyels

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Router from "./Router";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/Theme";
+import GlobalStyels from "../styles/GlobalStyles";
 
 export default () => (
   <ThemeProvider theme={theme}>
     <Router />
+    <GlobalStyels />
   </ThemeProvider>
 );

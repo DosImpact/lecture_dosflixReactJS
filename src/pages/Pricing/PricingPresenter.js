77d59ad8fb3b27e40120356d82c47db2d2f11fe1
DosImpact <ypd03008@gmail.com>
2020-02-03 14:01:55 +0900

    TabComponent

--- src/pages/Pricing/PricingPresenter.js
+++ src/pages/Pricing/PricingPresenter.js
@@ -1,3 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-export default () => "Pricing";
+export default () => {
+  const [index, setIndex] = useState(0);
+  return (
+    <div>
+      <Tabs selectedIndex={index} onSelect={index => setIndex(index)}>
+        <TabList>
+          <Tab>
+            <span>TAB1</span>
+          </Tab>
+          <Tab>
+            <span>TAB2</span>
+          </Tab>
+          <Tab>
+            <span>TAB3</span>
+          </Tab>
+        </TabList>
+        <div>now is {index}</div>
+        <TabPanel>
+          <div>IM ONE</div>
+        </TabPanel>
+        <TabPanel>
+          <div>IM TWO</div>
+        </TabPanel>
+        <TabPanel>
+          <div>IM THREE</div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};

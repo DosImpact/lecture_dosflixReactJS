57e3983df99ff52acfdc923ca380658a1ae4d9a6
DosImpact <ypd03008@gmail.com>
2020-02-17 12:00:17 +0900

    update DesignPattern

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Router from "./Router";
-import GlobalStyles from "./GlobalStyles";
+import GlobalStyles from "../Styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import Theme from "../Styles/Theme";
+
 function App() {
   return (
-    <>
-      <Router />
-      <GlobalStyles />
-    </>
+    <ThemeProvider theme={Theme}>
+      <>
+        <Router />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
   );
 }
 export default App;
